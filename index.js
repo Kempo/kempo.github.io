5698bdcece5d0ea22cd78b30183b9e4d6e96c11c
kempo <ilestkempo@gmail.com>
2018-03-28 11:40:46 -0700

    added randomclor.js and made fade animation smoother

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+/** random color library: https://github.com/davidmerfield/randomColor **/
+
 let index = 0;
-const t = ["high schooler", "hoodie lover", "food enthusiast", "basketball player", "programmer"];
+const t = ["high schooler", "pet lover", "food enthusiast", "basketball player", "programmer"];
 
 $(document).ready(function() {
 	var interval = window.setInterval(transitionTitles, 5000);
@@ -7,8 +9,8 @@ $(document).ready(function() {
 
 function transitionTitles() {
 	const newTitle = t[index];
-	setTitle(newTitle);
 
+	setTitle(newTitle);
 	const lastItem = t.length - 1;
 	if(index != lastItem) {
 		index += 1;
@@ -18,18 +20,25 @@ function transitionTitles() {
 }
 
 function setTitle(title) {
-	$("#title").css("color", randomColor(0));
 	$("#title").css("border-bottom-color", "black");
-	$("#title").text(title);
+
+	$("#title").animate({"opacity": 0}, 500, function() {
+		$("#title").css("color", randomColor({
+		luminosity: 'dark'	
+		}));
+		$(this).text(title).animate({"opacity": 1}, 500);
+	})
 }
 
-/** credits to David Mihal **/
+/** 
 function randomColor(brightness){
-  function randomChannel(brightness){
-    var r = 255-brightness;
-    var n = 0|((Math.random() * r) + brightness);
-    var s = n.toString(16);
-    return (s.length==1) ? '0'+s : s;
-  }
+  	function randomChannel(brightness){
+   	 	var r = 255-brightness;
+    	var n = 0|((Math.random() * r) + brightness);
+    	var s = n.toString(16);
+    	return (s.length==1) ? '0'+ s : s;
+  	}
+
   return '#' + randomChannel(brightness) + randomChannel(brightness) + randomChannel(brightness);
 }
+**/
